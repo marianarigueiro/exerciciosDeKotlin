ba5a46cdbe004a9b19869781f9608bf4f9767293
Mariana de O. Rigueiro <166918439+marianarigueiro@users.noreply.github.com>
2025-03-04 16:30:29 -0300

    Add files via upload

---
+++ kotlin-projetos/teste.kt
@@ -0,0 +1,24 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    
+    //Exercício 1 e 2
+    val name = "Mariana Rigueiro";
+    
+    println(name) //Comentário
+    
+    //Exercício 3
+    val a = 2;
+    val b = 4;
+    
+    val soma = (a + b);
+    println(soma)
+    val subtracao = (a - b);
+    println(subtracao)
+    val divisao = (a / b);
+    println(divisao)
+    val multiplicacao = (a * b);
+    println(multiplicacao)
+}
